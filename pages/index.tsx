0f7079b37607e0dac206a0d9b1fd11417ce672f4
Lee Jaeseong <fg9658@gmail.com>
2022-05-08 17:03:05 +0900

    test2

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+
   return (
     <div className={styles.container}>
       <Head>
@@ -16,6 +17,7 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <h2>{new Date().toLocaleString()}</h2>
 
         <p className={styles.description}>
           Get started by editing{' '}
